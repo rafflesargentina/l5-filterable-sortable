5f77da4db54a321e86ef697d0cfc5ec9d5d6f5d3
Mario Patronelli <mario@raffles.com.ar>
2018-12-20 21:40:51 +0000

    Update docs

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Exception"],["c","RafflesArgentina\\FilterableSortable\\Contracts\\FilterableInterface"],["c","RafflesArgentina\\FilterableSortable\\Contracts\\SortableInterface"],["c","RafflesArgentina\\FilterableSortable\\Exceptions\\FilterableSortableException"],["c","RafflesArgentina\\FilterableSortable\\FilterableSortableTrait"],["c","RafflesArgentina\\FilterableSortable\\QueryFilters"],["c","RafflesArgentina\\FilterableSortable\\QuerySorters"],["c","Throwable"]];
